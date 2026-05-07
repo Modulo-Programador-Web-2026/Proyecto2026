f4abc03b7988f52b86f2c47c5c4cf126be6a2f8c
Marcela Villanueva <villanuevacasinim@gmail.com>
2026-05-07 18:39:47 -0300

    FEAT:se crea serializer, viewsset y endpoints de Campania

--- backend/ProyectoMain/campanias/views.py
+++ backend/ProyectoMain/campanias/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Campania
+from .serializers import CampaniaSerializer
 
-# Create your views here.
+
+class CampaniaViewSet(viewsets.ModelViewSet):
+
+    queryset = Campania.objects.all()
+
+    serializer_class = CampaniaSerializer
