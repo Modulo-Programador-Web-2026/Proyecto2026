f32efccc1db30a24308a88755dccf27779ff63bd
Abigail Picone <abigailpicone.ispc@gmail.com>
2026-05-07 17:03:27 -0300

    FEAT: Se crea modelo Inscripcion

---
+++ backend/ProyectoMain/inscripciones/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from usuarios.models import Usuario
+from campanias.models import Campania
+
+# Create your models here.
+class Inscripcion(models.Model):
+        usuario = models.ForeignKey(
+        Usuario,
+        on_delete=models.CASCADE
+    )
+        campania = models.ForeignKey(
+        Campania,
+        on_delete=models.CASCADE
+    )
+        fecha_inscripcion = models.DateTimeField(auto_now_add=True)
+        
+        def __str__(self):
+            return f"{self.usuario} - {self.campania}"
+        
