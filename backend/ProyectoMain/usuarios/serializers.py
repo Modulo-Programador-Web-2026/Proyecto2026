2d1f16ac5a777b4cc10ee2913c5deb1c8caab870
astridlup <astrita1970@gmail.com>
2026-05-09 11:46:05 -0300

    FIX: Mejora modelos y serializers de usuarios

--- backend/ProyectoMain/usuarios/serializers.py
+++ backend/ProyectoMain/usuarios/serializers.py
@@ -2,8 +2,19 @@ from rest_framework import serializers
 
 from .models import Usuario
 
+
 class UsuarioSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Usuario
-        fields = '__all__'
+        fields = [
+            'id',
+            'username',
+            'email',
+            'dni',
+            'nombre',
+            'apellido',
+            'fecha_registro',
+            'rol',
+            'grupo_sanguineo'
+        ]
