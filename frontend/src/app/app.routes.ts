b5e54a7891803b0c759d2502b6250e980bf9fa9a
Irina Pirles <iripirles@gmail.com>
2026-05-22 20:23:31 -0300

    feat: se agrega componente Informate

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -11,6 +11,7 @@ import { Campanias } from './pages/public/campanias/campanias';
 import { Usuarios } from './pages/admin/usuarios/usuarios';
 import { Inscripciones } from './pages/public/inscripciones/inscripciones';
 import { CampaniaDetalle } from './pages/public/campania-detalle/campania-detalle';
+import { Informate } from './pages/public/informate/informate';
 
 
 
@@ -28,6 +29,7 @@ export const routes: Routes = [
             { path: 'campanias/:id/inscripcion', component: Inscripciones },
             { path: 'campanias/:id', component: CampaniaDetalle },
             { path: 'usuarios', component: Usuarios },
+            { path: 'informate', component: Informate },
 
 
         ]
